a09d6ec9d13ca4e0c1b7e6b86a0ff95ab93dbcca
King-Darshan-Rawan <darshanmalviya9826@gmail.com>
2025-07-12 15:30:51 +0530

    dependencies

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import './App.css'; 
 // Pages
 import Login from "./components/Login";
 import Signup from "./components/Signup";
