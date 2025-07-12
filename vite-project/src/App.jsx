8efd19d20e7e117685d5d60fde88d6dcd2102a9c
King-Darshan-Rawan <darshanmalviya9826@gmail.com>
2025-07-12 14:57:53 +0530

    go

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./components/Login";
-import Signup from "./components/Signup"; 
+// import Login from "./components/Login";
+// import Signup from "./components/Signup"; 
+// import Dashboard from "./pages/Dashboard";
+import CompleteProfile from "./components/CompleteProfile";
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        {/* <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} /> */}
+        {/* <Route path="/" element={<Dashboard />} /> */}
+        <Route path="/complete-profile" element={<CompleteProfile />} />
+
       </Routes>
     </Router>
   );
